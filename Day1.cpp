24ff2ba6353b4aaf763083208b51d27fb71bc5e8
Ankit Singh <2023pcecsankit019@poornima.org>
2025-08-17 14:01:38 +0530

    Update Day1.cpp

--- Day1.cpp
+++ Day1.cpp
@@ -5,3 +5,26 @@ Divide the array nums into n / 3 arrays of size 3 satisfying the following condi
 
 The difference between any two elements in one array is less than or equal to k.
 Return a 2D array containing the arrays. If it is impossible to satisfy the conditions, return an empty array. And if there are multiple answers, return any of them. */
+
+
+class Solution {
+public:
+    vector<vector<int>> divideArray(vector<int>& nums, int k) {
+        sort(nums.begin(), nums.end()); // step 1: Sort the array
+        vector<vector<int>> result;
+
+        for(int i=0; i<nums.size(); i += 3){
+            // Ensure we always pick 3 elements
+            int a = nums[i], b = nums[i+1], c = nums[i+2];
+
+            // step 2: check if max difference in group <=k
+            if(c - a <= k){
+                result.push_back({a,b,c});
+            } else {
+                return {}; // Invalid group found
+            }
+        }
+
+        return result;
+    }
+};
