3d2727934a65fc94e2dc24c92ab84f9ead262994
Ankit Singh <2023pcecsankit019@poornima.org>
2025-08-23 21:09:03 +0530

    Create Day3.cpp

---
+++ Day3.cpp
@@ -0,0 +1,39 @@
+/* 3443. Maximum Manhattan Distance After K Changes:
+You are given a string s consisting of the characters 'N', 'S', 'E', and 'W', where s[i] indicates movements in an infinite grid:
+
+'N' : Move north by 1 unit.
+'S' : Move south by 1 unit.
+'E' : Move east by 1 unit.
+'W' : Move west by 1 unit.
+Initially, you are at the origin (0, 0). You can change at most k characters to any of the four directions.
+
+Find the maximum Manhattan distance from the origin that can be achieved at any time while performing the movements in order.
+
+The Manhattan Distance between two cells (xi, yi) and (xj, yj) is |xi - xj| + |yi - yj|. */
+
+
+class Solution {
+public:
+    int maxDistance(string s, int k) {
+        int latitude = 0, longitude = 0, ans = 0;
+        int n = s.size();
+        for (int i = 0; i < n; i++) {
+            switch (s[i]) {
+                case 'N':
+                    latitude++;
+                    break;
+                case 'S':
+                    latitude--;
+                    break;
+                case 'E':
+                    longitude++;
+                    break;
+                case 'W':
+                    longitude--;
+                    break;
+            }
+            ans = max(ans, min(abs(latitude) + abs(longitude) + k * 2, i + 1));
+        }
+        return ans;
+    }
+};
