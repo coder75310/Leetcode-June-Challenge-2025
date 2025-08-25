b484f32d2c46192574ba9e037fdb054dee15fd8f
Ankit Singh <2023pcecsankit019@poornima.org>
2025-08-25 21:40:34 +0530

    Update Day7.cpp

--- Day7.cpp
+++ Day7.cpp
@@ -1,5 +1,8 @@
-2200. Find All K-Distant Indices in an Array:
+/* 2200. Find All K-Distant Indices in an Array:
 You are given a 0-indexed integer array nums and two integers key and k. A k-distant index is an index i of nums for which there exists
 at least one index j such that |i - j| <= k and nums[j] == key.
 
-Return a list of all k-distant indices sorted in increasing order.
+Return a list of all k-distant indices sorted in increasing order. */
+
+
+
