d66fbb07aa77a006bd79ed59cef87917a3a003cd
Ankit Singh <2023pcecsankit019@poornima.org>
2025-08-25 21:40:50 +0530

    Update Day7.cpp

--- Day7.cpp
+++ Day7.cpp
@@ -5,4 +5,23 @@ at least one index j such that |i - j| <= k and nums[j] == key.
 Return a list of all k-distant indices sorted in increasing order. */
 
 
+class Solution {
+public:
+    vector<int> findKDistantIndices(vector<int>& nums, int key, int k) {
+        vector<int> result;
+        int n = nums.size();
 
+        for(int i = 0; i < n; i++){
+            // Loop through every index j
+            for(int j = 0; j < n; j++){
+                // check the condition |i - j| <= k and nums[j] == key
+                if(abs(i - j) <= k && nums[j] == key){
+                    result.push_back(i);
+                    break; // No need to check more j's for this i 
+                }    
+            }
+        }
+
+        return result;
+    }
+};
