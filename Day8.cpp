0d34c351602363c69280aed3886491a6ae53e115
Ankit Singh <2023pcecsankit019@poornima.org>
2025-08-26 20:16:17 +0530

    Update Day8.cpp

--- Day8.cpp
+++ Day8.cpp
@@ -1,1 +1,3 @@
-
+2040. Kth Smallest Product of Two Sorted Arrays:
+Given two sorted 0-indexed integer arrays nums1 and nums2 as well as an integer k, return the kth (1-based) smallest product of
+nums1[i] * nums2[j] where 0 <= i < nums1.length and 0 <= j < nums2.length.
