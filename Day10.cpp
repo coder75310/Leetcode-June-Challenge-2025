ff1de76fe8d5a6e49aaf1da85bf48e28c37b2070
Ankit Singh <2023pcecsankit019@poornima.org>
2025-08-28 20:57:31 +0530

    Update Day10.cpp

--- Day10.cpp
+++ Day10.cpp
@@ -1,4 +1,4 @@
-2014. Longest Subsequence Repeated k Times :
+/* 2014. Longest Subsequence Repeated k Times :
 You are given a string s of length n, and an integer k. You are tasked to find the longest subsequence repeated k times in string s.
 
 A subsequence is a string that can be derived from another string by deleting some or no characters without changing the order of the 
@@ -10,4 +10,4 @@ concatenating seq k times.
 For example, "bba" is repeated 2 times in the string "bababcba", because the string "bbabba", constructed by concatenating "bba" 2 times,
 is a subsequence of the string "bababcba".
 Return the longest subsequence repeated k times in string s. If multiple such subsequences are found, return the lexicographically largest one.
-If there is no such subsequence, return an empty string.
+If there is no such subsequence, return an empty string. */
