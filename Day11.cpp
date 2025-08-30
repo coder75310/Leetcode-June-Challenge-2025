acd36acd0c207a141f2cade97f5ac40b20f58c9a
Ankit Singh <2023pcecsankit019@poornima.org>
2025-08-30 18:28:34 +0530

    Update Day11.cpp

--- Day11.cpp
+++ Day11.cpp
@@ -1,8 +1,8 @@
-1498. Number of Subsequences That Satisfy the Given Sum Condition:
+/* 1498. Number of Subsequences That Satisfy the Given Sum Condition:
 You are given an array of integers nums and an integer target.
 
 Return the number of non-empty subsequences of nums such that the sum of the minimum and maximum element on it is less or equal to target. Since the answer may be too large,
-return it modulo 10^9 + 7.
+return it modulo 10^9 + 7. */
 
  
 
