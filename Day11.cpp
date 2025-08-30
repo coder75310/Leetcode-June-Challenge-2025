ae9bbcd25c2efabb906a491991c7c8fe39ff5858
Ankit Singh <2023pcecsankit019@poornima.org>
2025-08-30 18:28:54 +0530

    Update Day11.cpp

--- Day11.cpp
+++ Day11.cpp
@@ -5,5 +5,32 @@ Return the number of non-empty subsequences of nums such that the sum of the min
 return it modulo 10^9 + 7. */
 
  
-
+class Solution {
+public:
+    int numSubseq(vector<int>& nums, int target) {
+        const int mod = 1e9 + 7;
+        int n = nums.size();
+        sort(nums.begin(), nums.end());
+        
+        // Precompute powers of 2
+        vector<int> pow2(n, 1);
+        for (int i = 1; i < n; ++i) {
+            pow2[i] = (pow2[i-1] * 2) % mod;
+        }
+        
+        int left = 0, right = n - 1;
+        int result = 0;
+        
+        while (left <= right) {
+            if (nums[left] + nums[right] <= target) {
+                result = (result + pow2[right - left]) % mod;
+                ++left;
+            } else {
+                --right;
+            }
+        }
+        
+        return result;
+    }
+};
 
