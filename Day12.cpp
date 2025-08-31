fe694f5464a1ced0c494ef7bfe43e47f23ad3d4d
Ankit Singh <2023pcecsankit019@poornima.org>
2025-08-31 21:10:00 +0530

    Update Day12.cpp

--- Day12.cpp
+++ Day12.cpp
@@ -1,4 +1,5 @@
-594. Longest Harmonious Subsequence:
+/* 594. Longest Harmonious Subsequence:
 We define a harmonious array as an array where the difference between its maximum value and its minimum value is exactly 1.
 
 Given an integer array nums, return the length of its longest harmonious subsequence among all its possible subsequences.
+ */
