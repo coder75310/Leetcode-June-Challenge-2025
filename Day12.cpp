0c55741191f830aa9dc75349b751799cd6313ef3
Ankit Singh <2023pcecsankit019@poornima.org>
2025-08-31 21:10:16 +0530

    Update Day12.cpp

--- Day12.cpp
+++ Day12.cpp
@@ -3,3 +3,22 @@ We define a harmonious array as an array where the difference between its maximu
 
 Given an integer array nums, return the length of its longest harmonious subsequence among all its possible subsequences.
  */
+
+
+class Solution {
+public:
+    int findLHS(vector<int>& nums) {
+        unordered_map<int, int> count;
+        for (int num : nums) {
+            count[num]++;
+        }
+
+        int maxLen = 0;
+        for (auto& [num, freq] : count) {
+            if (count.find(num + 1) != count.end()) {
+                maxLen = max(maxLen, freq + count[num + 1]);
+            }
+        }
+        return maxLen;
+    }
+};
